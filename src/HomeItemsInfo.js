ae53c43427c15f10177e0d1b2c193c1722b7ef9d
mdsou <80726254+mdsou@users.noreply.github.com>
2021-09-29 20:36:04 -0700

    Add files via upload

---
+++ src/HomeItemsInfo.js
@@ -0,0 +1,20 @@
+export const itemsInfo = [
+  {
+    id: 1,
+    title: "Item 1",
+    name: "Creator's Name",
+    info: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel esse aut, natus quod doloribus consequuntur ipsam beatae iure fugiat odio deleniti soluta eveniet, illum harum adipisci aspernatur error in explicabo?",
+  },
+  {
+    id: 2,
+    title: "Item 2",
+    name: "Creator's Name",
+    info: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel esse aut, natus quod doloribus consequuntur ipsam beatae iure fugiat odio deleniti soluta eveniet, illum harum adipisci aspernatur error in explicabo?",
+  },
+  {
+    id: 3,
+    title: "Item 3",
+    name: "Creator's Name",
+    info: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel esse aut, natus quod doloribus consequuntur ipsam beatae iure fugiat odio deleniti soluta eveniet, illum harum adipisci aspernatur error in explicabo?",
+  },
+];
